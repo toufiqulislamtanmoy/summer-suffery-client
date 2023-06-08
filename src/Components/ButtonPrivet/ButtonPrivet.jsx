37e59fbf8ba9d568c4d3b8df7fce66f930dffb76
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-06-08 12:32:53 +0600

    in the role is not user then button is disabled done and seats are not availabe then the also button is disable and bg color is red done

--- src/Components/ButtonPrivet/ButtonPrivet.jsx
+++ src/Components/ButtonPrivet/ButtonPrivet.jsx
@@ -1,27 +1,42 @@
-
-
-import { useContext } from "react";
+import { useContext ,useState,useEffect} from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import {useLocation, useNavigate } from "react-router-dom";
 
-const ButtonPrivet = () => {
+
+const ButtonPrivet = ({ userRole, seats }) => {
   const { user } = useContext(AuthContext);
+  const [btnDisable, setBtnDisable] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
+
   const handleButtonClick = () => {
     if (!user) {
       // Redirect to login page if user is not logged in
       navigate("/login", { state: { from: location }, replace: true });
-    }else{
-      console.log("Class is selected")
-    } 
+    } else {
+      console.log("Class is selected");
+    }
   };
 
+  // Check if userRole is "admin" or "instructor" and seats is 0
+  useEffect(() => {
+    if (userRole === "admin" || userRole === "instructor" || seats === 0) {
+      setBtnDisable(true);
+    } else {
+      setBtnDisable(false);
+    }
+  }, [userRole, seats]);
+
   return (
-    <button onClick={handleButtonClick} className="btn btn-primary">
+    <button
+      onClick={handleButtonClick}
+      className="btn btn-primary"
+      disabled={btnDisable}
+    >
       Select Class
     </button>
   );
 };
 
 export default ButtonPrivet;
+
--- src/Pages/Classes/ClassesCard/ClassesCard.jsx
+++ src/Pages/Classes/ClassesCard/ClassesCard.jsx
@@ -1,9 +1,15 @@
 import ButtonPrivet from "../../../Components/ButtonPrivet/ButtonPrivet";
+import { useContext, useEffect,useState } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 
 const ClassesCard = ({ approvedClasses }) => {
+    const { user } = useContext(AuthContext);
+    const [userRole,setUserRole] = useState("")
+    useEffect(()=>{
+        fetch(`http://localhost:5000/users/${user?.email}`).then(res => res.json()).then(data => setUserRole(data.role))
+    },[user])
     const { price, seats, instructor, name, image } = approvedClasses;
-    console.log(approvedClasses)
     return (
         <div className={`card w-96 ${seats === 0 ? 'bg-red-500 text-white' : 'bg-base-100'} shadow-xl`}>
             <figure><img src={image} alt="not found" /></figure>
@@ -18,7 +24,7 @@ const ClassesCard = ({ approvedClasses }) => {
                 <p><span>Remaining seats :</span>{seats}</p>
                 <p><span>Price :</span>{price} TK</p>
                 <div className="card-actions justify-end">
-                    <ButtonPrivet />
+                    <ButtonPrivet userRole={userRole} seats={seats}/>
                 </div>
             </div>
         </div>
